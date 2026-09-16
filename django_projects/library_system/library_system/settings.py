6c5d6de33179e1c3a2d5e0810d4d81c3b2f7032c
arthurleywin1505 <subill1504@gmail.com>
2026-09-16 14:42:31 +0530

    Update settings.py

--- django_projects/library_system/library_system/settings.py
+++ django_projects/library_system/library_system/settings.py
@@ -77,10 +77,11 @@ WSGI_APPLICATION = "library_system.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+   'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
