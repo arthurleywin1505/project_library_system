198cac92d12fd2f939b522a39ae234f152422ec7
arthurleywin1505 <subill1504@gmail.com>
2026-09-16 14:50:03 +0530

    Update settings.py

--- django_projects/library_system/library_system/settings.py
+++ django_projects/library_system/library_system/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "library_system.wsgi.application"
 
 DATABASES = {
    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=os.environ.get('postgresql://neondb_owner:npg_Jt3LEjNFfZd5@ep-cold-sky-aza2dool.c-3.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'),
         conn_max_age=600,
         conn_health_checks=True,
     )
