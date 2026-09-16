2c6c246e40c9f7048750d4ef7407b08b8a9a790b
arthurleywin1505 <subill1504@gmail.com>
2026-09-16 14:19:24 +0530

    Update Django settings for library_system project

--- django_projects/library_system/library_system/settings.py
+++ django_projects/library_system/library_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-8$nwnueq!rjzesgkv8e6p89+x_j%g+0t27ak90emyo+(z0-8*p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,7 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
